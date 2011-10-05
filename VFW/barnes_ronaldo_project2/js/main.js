aea0b55fed50fde310622d9704ebc8690ce63bbe
Ronaldo Barnes <krazydragon@fullsail.edu>
2011-10-05 08:34:17 -0700

    Populater function working :)

--- VFW/barnes_ronaldo_project2/js/main.js
+++ VFW/barnes_ronaldo_project2/js/main.js
@@ -1,3 +1,42 @@
 // Project 2
 // Author: Ronaldo Barnes
 // Created for: VFW Online 1110
+
+
+// wait until DOM loads
+window.addEventListener("DOMContentLoaded", function() {
+
+	//Retrieve element function
+	function $(k) {
+		var theElm = document.getElementById(k);
+		return theElm;
+	}
+
+	//Restaurant Populater function
+	function createRestTypes() {
+		var formTag = document.getElementsByTagName("form"),
+			pickLi = $("pick"),
+			createType = document.createElement("select");
+			createType.setAttribute("id","types");
+			for (var b=0, c=restTypes.length; b<c; b++){
+				var createOption = document.createElement("option");
+				var optText = restTypes[b];
+				createOption.setAttribute("value", optText);
+				createOption.innerHTML = optText;
+				createType.appendChild(createOption);
+			}
+			pickLi.appendChild(createType);
+		}
+
+	//Varibles 
+	
+	var restTypes = [ "---Pick A Restaurant Type---", "Sports Themed", "Bar/Club", "Outside", "Other" ];
+	createRestTypes();
+	
+	
+
+
+
+
+
+});
