aa1ef228b13a742a292548719c91af800e337a85
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-01-06 18:11:33 -0800

    Revert 928ce5160e57a775e624179cb7e4c081824d5994^..HEAD

--- ASD/barnes_ronaldo_project1/js/main.js
+++ ASD/barnes_ronaldo_project1/js/main.js
@@ -9,8 +9,7 @@ $(function(){
 	
 	//Varibles 
 	var info = new Array(),
-		restInfo = ("place", "date", "types", "food", "numScale", "comments")
-	;
+		ID = "";
 	
 	
 	//JSON Object
@@ -212,15 +211,16 @@ $(function(){
 		}
 
 		for(var n in json){	
-			var num = Math.floor(Math.random()*7463778270);
+			var num = Math.floor(Math.random()*100000);
 			localStorage.setItem(num,  JSON.stringify(json[n]));
 		}
 	}
 		
 	// Displays the saved data in local storage on the screen
 	function viewData(info, BUTTON){
+		$(BUTTON +'page').empty();
 		for (var d=0; d<info.length; d++){	
-			var newDiv = $('<div></div>'),
+			var newDiv = $('<div></div>').attr("data-role", "collapsible"),
 				newH3 = $('<h3></h3>'),
 				newP1 = $('<p></p>'),
 				newP2 = $('<p></p>'),
@@ -228,6 +228,8 @@ $(function(){
 				newP4 = $('<p></p>'),
 				newP5 = $('<p></p>'),
 				newP6 = $('<p></p>');
+				//newA1 = $('<a href="#" id="editButton" data-role="button" data-icon="back">Edit</a>').bind('click', editEntry(ID)),
+				//newA2 = $('<a href="#" id="deleteButton" data-role="button" data-icon="delete">Delete</a>').bind('click', deleteEntry(ID));
 			$(newH3).html(info[d].restaurant);
 			$(newP1).html("Location : " + info[d].place);
 			$(newP2).html("Date : " + info[d].date);
@@ -235,39 +237,43 @@ $(function(){
 			$(newP4).html("Kind of food served : " + info[d].food);
 			$(newP5).html("On a scale of 1-10 how good was it? : " + info[d].numScale);
 			$(newP6).html("Comments : " + info[d].comments);
-			$(newDiv).attr("data-role", "collapsible").append(newH3, newP1, newP2, newP3, newP4, newP5, newP6);
+			$(newDiv).append(newH3, newP1, newP2, newP3, newP4, newP5, newP6/*, newA1, newA2*/);
 			$(BUTTON +'page').append(newDiv);
-			
-			
 		}	
-
 		
 	}
-
+	
+	/*function editEntry(ID){
+		console.log(ID);
+	}
+	
+	function deleteEntry(){
+	
+	}*/
+	
 	//Poulate correct info when button is pressed
 	function processLocal(Button){
 		if(localStorage.length === 0){
 			autoLoadData();
 		}
-		var restArr= new Array();
-		for(d=0; d<localStorage.length; d++){;
+		for(var d=0; d<localStorage.length; d++){
 			var key = localStorage.key(d),
 				val = localStorage.getItem(key),
 				v = JSON.parse(val);
-			restArr.push(v);						
+				info.push(v);
+										
 		}
-		return info = restArr;	
+		return info;	
 	}
 	
 	//Picks proper groups
 	function getTypes(v, b){
-		var restArr= new Array()
-		for(d=0; d<info.length; d++){
+		for(var d=0; d<info.length; d++){
 			if(b === v[d].types){
-				restArr.push(v[d]);
+				info.push(v[d]);
 			}
 		}
-		return info = restArr;
+		return info;
 	}
 	
 	//Sorts info
@@ -275,18 +281,6 @@ $(function(){
 		var sortArr = info.sort(u);
 		viewData(info, BUTTON);
 		
-	}
-	
-	function today(){
-		var now = new Date(),
-   			year =now.getFullYear(),
-   			month = now.getMonth() + 1, 
-   			day = now.getDate();
-   		if(day < 10){
-   			day = "0" + day
-   		}
-		var rv = year + "-" + month + "-" + day
-		return rv;
 	}
 	
 	//Populates Family Group Screen
@@ -331,9 +325,6 @@ $(function(){
 	}
 	
 	
-	function addItems(){
-		$('#date').val() = today();
-	}
 
 	//Wipe local storage
 	function emptyStorage(){
@@ -342,15 +333,8 @@ $(function(){
 		}else{
 			localStorage.clear();
 			window.location.reload();
-			return false;
 		}
 	}	
-	
-	function parseRestForm(info){
-		console.log(info)
-		alert("Restaurant Tracked!");
-		$("#mainLink").click();
-		}
 	
 	
 	$("#restForm").validate({
@@ -364,8 +348,10 @@ $(function(){
 				"numScale" : $('#numScale').val(),
 				"comments" : $('#comments').val()
 			}
-			var num = Math.floor(Math.random()*7463778270);
+			var num = Math.floor(Math.random()*1000000);
 			localStorage.setItem(num,  JSON.stringify(Restaurant));
+			alert("Restaurant Tracked!");
+			$('#mainLink').click();
 		}
 	})
 	
@@ -375,8 +361,8 @@ $(function(){
 	$("#SportsButton").bind('click', sButton);
 	$("#OtherButton").bind('click', OButton);
 	$("#AllButton").bind('click', aButton);
-	$("#addEntry").bind('click', addItems);
 	$("#wipeInfo").bind('click', emptyStorage);
+	
 
 });
 	
