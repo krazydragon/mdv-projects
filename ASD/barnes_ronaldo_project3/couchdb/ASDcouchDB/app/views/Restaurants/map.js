086d93160c3ef75a35ad55fdfae857e1c23efc26
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-01-16 06:56:23 -0800

    did map instead

--- ASD/barnes_ronaldo_project3/couchdb/ASDcouchDB/app/views/Restaurants/map.js
+++ ASD/barnes_ronaldo_project3/couchdb/ASDcouchDB/app/views/Restaurants/map.js
@@ -1,6 +1,6 @@
 function (doc){
 	if(doc._id.substr(0,10) === "Restaurant"){
-		emit(doc.id.substr(10),{
+		emit(doc._id.substr(10),{
 			"place": doc.place,
 			"restaurant": doc.restaurant,
 			"date": doc.date,
