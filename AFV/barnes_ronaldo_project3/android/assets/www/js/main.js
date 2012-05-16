5c7561f3d1accf72c0ce200c326780231b6fcff4
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-05-15 22:38:13 -0700

    Added camera
    tested on android phone can't test on iPad yet :(

--- AFV/barnes_ronaldo_project3/android/assets/www/js/main.js
+++ AFV/barnes_ronaldo_project3/android/assets/www/js/main.js
@@ -1,10 +1,10 @@
-// Project 2
+// Project 3
 // Author: Ronaldo Barnes
 // Created for: AVF Online 0205
 
+var destinationType;
+document.addEventListener("deviceready", onDeviceReady, false);
 
-	document.addEventListener("deviceready", onDeviceReady, false);
-	
    
     function onDeviceReady() {
     	
@@ -22,7 +22,7 @@
             'I work!',  
             alertDismissed,         
             'Alert',            
-            'Back'                  
+            'Click'                  
         );
     } 
     $('#Geolocation').live("pageshow", function(){
@@ -31,17 +31,37 @@
         
     });
 
-    function displayMap(position){       
-           var lat = position.coords.latitude,
-           long = position.coords.longitude,
-           gpsData = (lat +","+ long);
-       
-       alert(gpsData);
-       $('#map_canvas').gmap({'zoom': 15,'center': gpsData});
-   }
-                        
-          
+    function displayMap(position){
+        var lat = position.coords.latitude,
+            long = position.coords.longitude,
+            gpsData = (lat +","+ long);
+        
+        alert(gpsData);
+        $('#map_canvas').gmap({'zoom': 8,'center': gpsData});
+    }
 
     function noMap(error) {
         alert('Can\'t retrieve position.\nError: ' + error);
     };
+
+    $('#Camera').live("pageshow", function(){
+    	pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    });
+
+
+    function getPhoto(imageData) {
+        var demoImage = $('#demoImage');
+    
+        demoImage.style.display = 'block';
+    
+        demoImage.src = imageData;
+    }
+
+    function capturePhoto() {
+        navigator.camera.getPicture(getPhoto, camFail, { quality: 50, allowEdit: true, destinationType: navigator.camera.DestinationType.FILE_URI});
+    }
+
+    function camFail(error) {
+        alert('Failed because: ' + error);
+    }
--- AFV/barnes_ronaldo_project3/ios/testApp/www/js/main.js
+++ AFV/barnes_ronaldo_project3/ios/testApp/www/js/main.js
@@ -2,44 +2,66 @@
 // Author: Ronaldo Barnes
 // Created for: AVF Online 0205
 
-
+var destinationType;
 document.addEventListener("deviceready", onDeviceReady, false);
 
-   
-    function onDeviceReady() {
-    	
-    }
+
+function onDeviceReady() {
     
+}
+
+
+function alertDismissed() {
     
-    function alertDismissed() {
-        
-    }
+}
 
-   
-    // Show alert 
-    function showAlert() {
-        navigator.notification.alert(
-            'I work!',  
-            alertDismissed,         
-            'Alert',            
-            'Click'                  
-        );
-    } 
-    $('#Geolocation').live("pageshow", function(){
-        navigator.geolocation.getCurrentPosition(displayMap, noMap);
-                           
-        
-    });
 
-    function displayMap(position){
-        var lat = position.coords.latitude,
-            long = position.coords.longitude,
-            gpsData = (lat +","+ long);
-        
-        alert(gpsData);
-        $('#map_canvas').gmap({'zoom': 8,'center': gpsData});
-    }
+// Show alert 
+function showAlert() {
+    navigator.notification.alert(
+                                 'I work!',  
+                                 alertDismissed,         
+                                 'Alert',            
+                                 'Click'                  
+                                 );
+} 
+$('#Geolocation').live("pageshow", function(){
+                       navigator.geolocation.getCurrentPosition(displayMap, noMap);
+                       
+                       
+                       });
 
-    function noMap(error) {
-        alert('Can\'t retrieve position.\nError: ' + error);
-    };
+function displayMap(position){
+    var lat = position.coords.latitude,
+    long = position.coords.longitude,
+    gpsData = (lat +","+ long);
+    
+    alert(gpsData);
+    $('#map_canvas').gmap({'zoom': 8,'center': gpsData});
+}
+
+function noMap(error) {
+    alert('Can\'t retrieve position.\nError: ' + error);
+};
+
+$('#Camera').live("pageshow", function(){
+                  pictureSource=navigator.camera.PictureSourceType;
+                  destinationType=navigator.camera.DestinationType;
+                  });
+
+
+function getPhoto(imageData) {
+    var demoImage = $('#demoImage');
+    
+    demoImage.style.display = 'block';
+    
+    demoImage.src = imageData;
+}
+
+function capturePhoto() {
+    navigator.camera.getPicture(getPhoto, camFail, { quality: 50, allowEdit: true, destinationType: navigator.camera.DestinationType.FILE_URI});
+}
+
+function camFail(error) {
+    alert('Failed because: ' + error);
+}
